a48b7fe9d21c81bb253b013dcf970fe71b326007
Kayleen Cucugliello <kcucugliello@outlook.com>
2026-01-18 20:08:24 -0500

    fix: force relative paths for assets

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // This ensures paths are relative and work on Vercel/GitHub
+  // Use './' to make all paths relative. 
+  // This is the most "fail-safe" way for GitHub Pages and Vercel.
+  base: './', 
 })
